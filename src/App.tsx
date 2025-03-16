95eea0c32e7aecc1c04e88cbaf7e8d1964a7dcde
Vitor Ramires <vmr.rs@hotmail.com>
2025-03-15 21:08:50 -0300

    new layout on working

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
-import { ThemeProvider } from "styled-components";
-import { Header } from "./components/header";
-import { themeGlobal } from "./styles/themes/themeGlobal";
-import { Centralize, GlobalStyle } from "./styles/style";
-import { Banner } from "./components/banner";
-import { Projects } from "./components/projects";
-import { Skills } from "./components/skills";
-import { Experiences } from "./components/experiences";
-
 function App() {
   return (
     <>
-      <ThemeProvider theme={themeGlobal}>
-        <GlobalStyle />
-        <Header />
-        <Banner />
-        <Centralize>
-          <Skills />
-          <Projects />
-          <Experiences />
-        </Centralize>
-      </ThemeProvider>
+      <h2>Sendo refeito!</h2>
     </>
   );
 }
