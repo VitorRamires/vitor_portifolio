babae767cd163890e470e6cade5a27d749019baf
Vitor Ramires <vmr.rs@hotmail.com>
2025-06-24 19:34:00 -0300

    new: responsive

--- src/components/experiences.tsx
+++ src/components/experiences.tsx
@@ -1,14 +1,15 @@
 import { Title } from "./title";
 import { Experience } from "./experience.tsx";
 
+
 export function Experiences() {
   return (
     <section id="experiences">
       <div className="center">
         <Title synonim="Trajetória" title="Experiências" />
         <div className="experiences-wrapper">
-          <div className="timeline"></div>
           <Experience />
+          <div className="timeline"></div>
         </div>
       </div>
     </section>
